a9c2d283098b9ffab4b7dbebab6af3027ab6b13e
Andrew <Andrew.Stephenson123@gmail.com>
2014-11-16 00:58:17 -0700

    Got a skeleton frontend up using angular, bootstrap, grunt, yo, and bower. Look at the readme for instructions on how to get it running on your system. Also note that the brief designs that I did are not at all mobile-friendly, and are intended as placeholders so that everybody could have something to look at when they get the app up and running, and not as something to build off of later.

---
+++ client/app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .run(['$rootScope', '$location', function($rootScope, $location) {
+    $rootScope.changeRoute = function(newRoute) {
+      $location.url(newRoute);
+    }
+  }]);
