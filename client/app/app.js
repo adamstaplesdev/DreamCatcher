5a888b561d9f1c9cddc8230190f5a6ed82e033ce
fudge22 <fudge22it@gmail.com>
2014-12-08 19:00:23 -0700

    updated styles

--- client/app/app.js
+++ client/app/app.js
@@ -80,11 +80,14 @@ angular.module('dreamCatcherApp', [
 
       $rootScope.currentRoute = next.url;
       if (next.url === '/overview') {
-        $rootScope.viewportWidth = 'col-lg-12';
+			$rootScope.viewportWidth = 'col-lg-12';
     		$rootScope.overviewWrapper = "overview-wrapper"
     		$rootScope.containerPadding = {'padding':'0px'}
     		$rootScope.breadcrumbs = "hide"
       }
+	  else if (next.url === '/signup'){
+			$rootScope.breadcrumbs = "hide"
+	  }
       else {
         $rootScope.viewportWidth = 'col-lg-10';
         $rootScope.overviewWrapper = ""
