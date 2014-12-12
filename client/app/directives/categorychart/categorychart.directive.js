d4e6ac79f785603c1216912a84bee1024acf2087
Adam Staples <bent.paperclip27@outlook.com>
2014-12-11 19:40:07 -0700

    Categories piechart complete, dynacmically updates with number of dreams in the category

--- client/app/directives/categorychart/categorychart.directive.js
+++ client/app/directives/categorychart/categorychart.directive.js
@@ -11,41 +11,37 @@ angular.module('dreamCatcherApp')
 		scope:{},
 		
 		controller: function($scope, dreamFactory){
-			//CURRENTLY HARD CODED WITH A FEW CATEGORIES
-			//See sample code at the bottom of the page
-			//As soon as I can get this on the page, I'll work on getting dynamic categories.
-			var colors = ["#57b813","#a1650a","#248838"];
-			var colorIndex = 0;
-			$scope.content = [];
 			
-			console.log(dreamFactory);
-			dreamFactory.getUserCategories().then(function(categoryList){
-				console.log('CATEGORY DATA:');
-				console.log(categoryList);
-				for(var categoryIndex in categoryList){
-					$scope.content.push({
-						"label": categoryList[categoryIndex],
-						"value": 1,
-						"color": colors[colorIndex % 3]
+			var colors = ["#397B31","#482915","#C77846","#4BC73C","#4A4488"];
+			var colorIndex = 0;
+			
+			dreamFactory.getDreams().then(function(dreams){
+				console.log(dreams);
+				var categories = {};
+				var names = [];
+				for(var index in dreams){
+					if(categories[dreams[index].category]){
+						categories[dreams[index].category] = categories[dreams[index].category] + 1;
+					}
+					else{
+						names.push(dreams[index].category);
+						categories[dreams[index].category] = 1;
+					}
+					console.log(categories);
+				}
+				var content = [];
+				for(var index in names){
+					console.log(names[index]);
+					content.push({
+						"label": names[index],
+						"value": categories[names[index]],
+						"color": colors[colorIndex % colors.length]
 					});
 					colorIndex++;
 				}
-				console.log($scope.content);
+				console.log("New Category Data:");
+				console.log(content);
 				$scope.chart = new d3pie("categorychart", {
-					/*
-					"header": {
-						"title": {
-							"text": "Categories",
-							"fontSize": 24,
-							"font": "open sans"
-						}//,
-						//"subtitle": {
-							//"color": "#999999",
-							//"fontSize": 12,
-							//"font": "open sans"
-						//},
-						//"titleSubtitlePadding": 9
-					},*/
 					"footer": {
 						"color": "#999999",
 						"fontSize": 10,
@@ -53,28 +49,12 @@ angular.module('dreamCatcherApp')
 						"location": "bottom-left"
 					},
 					"size": {
-						"canvasWidth": 400
+						"canvasWidth": 500,
+						"pieOuterRadius": 140
 					},
 					"data": {
 						"sortOrder": "value-desc",
-						"content": $scope.content
-						/*[
-							{
-								"label": "Financial",
-								"value": 4,
-								"color": "#57b813"
-							},
-							{
-								"label": "Spiritual",
-								"value": 2,
-								"color": "#a1650a"
-							},
-							{
-								"label": "Health",
-								"value": 5,
-								"color": "#248838"
-							}
-						]*/
+						"content": content
 					},
 					"labels": {
 						"outer": {
@@ -99,18 +79,21 @@ angular.module('dreamCatcherApp')
 						},
 						"lines": {
 							"enabled": false
+						},
+						"truncation": {
+							"enabled": true,
+							"length": 10
 						}
-					},
+					}/*,
 					"effects": {
 						"pullOutSegmentOnClick": {
 							"effect": "linear",
 							"speed": 400,
 							"size": 8
 						}
-					}
+					}*/
 				});
 			});
-			//NOTE: getCategories only gets the names, not how many dreams have that category.
 			
 		}
 
@@ -118,90 +101,3 @@ angular.module('dreamCatcherApp')
 		//}
 	};
 });
-
-/*
-//SAMPLE CHART CODE
-<div id="pieChart"></div>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.4/d3.min.js"></script>
-<script src="d3pie.min.js"></script>
-<script>
-var pie = new d3pie("pieChart", {
-	"header": {
-		"title": {
-			"text": "Categories",
-			"fontSize": 24,
-			"font": "open sans"
-		},
-		"subtitle": {
-			"color": "#999999",
-			"fontSize": 12,
-			"font": "open sans"
-		},
-		"titleSubtitlePadding": 9
-	},
-	"footer": {
-		"color": "#999999",
-		"fontSize": 10,
-		"font": "open sans",
-		"location": "bottom-left"
-	},
-	"size": {
-		"canvasWidth": 590
-	},
-	"data": {
-		"sortOrder": "value-desc",
-		"content": [
-			{
-				"label": "Financial:",
-				"value": 4,
-				"color": "#57b813"
-			},
-			{
-				"label": "Spiritual:",
-				"value": 2,
-				"color": "#a1650a"
-			},
-			{
-				"label": "Health:",
-				"value": 5,
-				"color": "#248838"
-			}
-		]
-	},
-	"labels": {
-		"outer": {
-			"format": "none",
-			"pieDistance": 0
-		},
-		"inner": {
-			"format": "label-value1",
-			"hideWhenLessThanPercentage": 3
-		},
-		"mainLabel": {
-			"color": "#ffffff",
-			"fontSize": 18
-		},
-		"percentage": {
-			"color": "#ffffff",
-			"decimalPlaces": 0
-		},
-		"value": {
-			"color": "#e9bfbf",
-			"fontSize": 18
-		},
-		"lines": {
-			"enabled": true
-		}
-	},
-	"effects": {
-		"pullOutSegmentOnClick": {
-			"effect": "linear",
-			"speed": 400,
-			"size": 8
-		}
-	}
-});
-</script>
-
-*/
